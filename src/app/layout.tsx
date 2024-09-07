f95eb1fb1140571eafbf765c513c8f1fe0dd3469
Farhan Masood <84273944+farhanmasood-se@users.noreply.github.com>
2024-09-07 22:23:31 +0500

    fix jotai duplication error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
+import { ConvexClientProvider } from '@/components/convex-client-provider';
+import JotaiProvider from '@/components/jotai-provider';
+import { Modals } from '@/components/modals';
+import { Toaster } from '@/components/ui/sonner';
+import { ConvexAuthNextjsServerProvider } from '@convex-dev/auth/nextjs/server';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { ConvexClientProvider } from '@/components/convex-client-provider';
-import { ConvexAuthNextjsServerProvider } from '@convex-dev/auth/nextjs/server';
-import { Modals } from '@/components/modals';
-import { Toaster } from '@/components/ui/sonner';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,9 +24,11 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <ConvexClientProvider>
-            <Toaster />
-            <Modals />
-            {children}
+            <JotaiProvider>
+              <Toaster />
+              <Modals />
+              {children}
+            </JotaiProvider>
           </ConvexClientProvider>
         </body>
       </html>
