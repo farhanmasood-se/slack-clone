f8ed48290e875bff4338a4198fc38c9efffd0906
Farhan Masood <84273944+farhanmasood-se@users.noreply.github.com>
2024-09-08 11:08:35 +0500

    update workspace

--- src/app/workspace/[workspaceId]/_components/preferences-model.tsx
+++ src/app/workspace/[workspaceId]/_components/preferences-model.tsx
@@ -1,11 +1,20 @@
+import { Button } from '@/components/ui/button';
 import {
   Dialog,
+  DialogClose,
   DialogContent,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
+  DialogTrigger,
 } from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { useRemoveWorkspace } from '@/features/workspaces/api/use-remove-workspace';
+import { useUpdateWorkspace } from '@/features/workspaces/api/use-update-workspace';
+import { useWorkspaceId } from '@/hooks/use-workspace-id';
 import { TrashIcon } from 'lucide-react';
 import { useState } from 'react';
+import { toast } from 'sonner';
 
 interface PreferencesModelProps {
   open: boolean;
@@ -18,7 +27,32 @@ const PreferencesModel = ({
   setOpen,
   initialValue,
 }: PreferencesModelProps) => {
+  const workspaceId = useWorkspaceId();
+
   const [value, setValue] = useState(initialValue);
+  const [editOpen, setEditOpen] = useState(false);
+
+  const { mutate: updateWorkspace, isPending: isUpdatingWorkspace } =
+    useUpdateWorkspace();
+  const { mutate: removeWorkspace, isPending: isRemovingWorkspace } =
+    useRemoveWorkspace();
+
+  const handelEdit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    updateWorkspace(
+      { id: workspaceId, name: value },
+      {
+        onSuccess: () => {
+          setEditOpen(false);
+          toast.success('Workspace updated successfully');
+        },
+        onError: (error) => {
+          toast.error('Failed to update workspace');
+        },
+      },
+    );
+  };
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -28,15 +62,44 @@ const PreferencesModel = ({
         </DialogHeader>
 
         <div className="px-4 pb-4 flex flex-col gap-y-2">
-          <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
-            <div className="flex items-center justify-between">
-              <p className="text-sm font-semibold">Workspace Name</p>
-              <p className="text-sm text-[#1264a3] hover:underline font-semibold">
-                Edit
-              </p>
-            </div>
-            <p className="text-sm">{value}</p>
-          </div>
+          <Dialog open={editOpen} onOpenChange={setEditOpen}>
+            <DialogTrigger asChild>
+              <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
+                <div className="flex items-center justify-between">
+                  <p className="text-sm font-semibold">Workspace Name</p>
+                  <p className="text-sm text-[#1264a3] hover:underline font-semibold">
+                    Edit
+                  </p>
+                </div>
+                <p className="text-sm">{value}</p>
+              </div>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Rename this workspace</DialogTitle>
+              </DialogHeader>
+              <form className="space-y-4" onSubmit={handelEdit}>
+                <Input
+                  value={value}
+                  onChange={(e) => setValue(e.target.value)}
+                  disabled={isUpdatingWorkspace}
+                  required
+                  autoFocus
+                  minLength={3}
+                  maxLength={80}
+                  placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
+                />
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button variant="outline" disabled={isUpdatingWorkspace}>
+                      Cancel
+                    </Button>
+                  </DialogClose>
+                  <Button disabled={isUpdatingWorkspace}>Save</Button>
+                </DialogFooter>
+              </form>
+            </DialogContent>
+          </Dialog>
 
           <button
             disabled={false}
