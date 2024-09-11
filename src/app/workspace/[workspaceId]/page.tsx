f212d3fd2caccb8eb787d601fb70c6289a57e49c
Farhan Masood <84273944+farhanmasood-se@users.noreply.github.com>
2024-09-11 10:57:17 +0500

    fixes in create channel modal and workspace id page

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -2,6 +2,7 @@
 
 import { useGetChannels } from '@/features/channels/api/use-get-channels';
 import { useCreateChannelModal } from '@/features/channels/store/use-create-channel-modal';
+import { useCurrentMember } from '@/features/members/api/use-current-member';
 import { useGetWorkspace } from '@/features/workspaces/api/use-get-workspace';
 import { useWorkspaceId } from '@/hooks/use-workspace-id';
 import { Loader, TriangleAlert } from 'lucide-react';
@@ -13,6 +14,9 @@ const WorkspaceIdPage = () => {
   const workspaceId = useWorkspaceId();
   const [open, setOpen] = useCreateChannelModal();
 
+  const { data: member, isLoading: memberLoading } = useCurrentMember({
+    workspaceId,
+  });
   const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({
     id: workspaceId,
   });
@@ -21,16 +25,27 @@ const WorkspaceIdPage = () => {
   });
 
   const channelId = useMemo(() => channels?.[0]?._id, [channels]);
+  const isAdmin = useMemo(() => member?.role === 'admin', [member?.role]);
 
   useEffect(() => {
-    if (workspaceLoading || channelsLoading || !workspace) return;
+    if (
+      workspaceLoading ||
+      channelsLoading ||
+      memberLoading ||
+      !member ||
+      !workspace
+    )
+      return;
 
     if (channelId) {
       router.push(`/workspace/${workspaceId}/channel/${channelId}`);
-    } else if (!open) {
+    } else if (!open && isAdmin) {
       setOpen(true);
     }
   }, [
+    member,
+    memberLoading,
+    isAdmin,
     channelId,
     workspaceLoading,
     channelsLoading,
@@ -52,7 +67,7 @@ const WorkspaceIdPage = () => {
   if (!workspace) {
     return (
       <div className="h-full flex-1 flex items-center justify-center flex-col gap-2">
-        <TriangleAlert className="size-6 animate-spin text-muted-foreground" />
+        <TriangleAlert className="size-6 text-muted-foreground" />
         <span className="text-sm text-muted-foreground">
           Workspace not found
         </span>
@@ -60,7 +75,12 @@ const WorkspaceIdPage = () => {
     );
   }
 
-  return null;
+  return (
+    <div className="h-full flex-1 flex items-center justify-center flex-col gap-2">
+      <TriangleAlert className="size-6 text-muted-foreground" />
+      <span className="text-sm text-muted-foreground">Channel not found</span>
+    </div>
+  );
 };
 
 export default WorkspaceIdPage;
