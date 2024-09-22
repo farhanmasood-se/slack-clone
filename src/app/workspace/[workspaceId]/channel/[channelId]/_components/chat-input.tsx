04554ee9cf304988b403d986bf06e2f7687f469f
Farhan Masood <84273944+farhanmasood-se@users.noreply.github.com>
2024-09-22 12:02:56 +0500

    handleSubmit function, implement into the editor and implement it to the previous todo in the handler

--- src/app/workspace/[workspaceId]/channel/[channelId]/_components/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/_components/chat-input.tsx
@@ -1,8 +1,8 @@
-import dynamic from 'next/dynamic';
-import Quill from 'quill';
-import { useRef } from 'react';
+import dynamic from "next/dynamic";
+import Quill from "quill";
+import { useRef } from "react";
 
-const Editor = dynamic(() => import('@/components/editor'), { ssr: false });
+const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 
 interface ChatInputProps {
   placeholder: string;
@@ -11,11 +11,21 @@ interface ChatInputProps {
 export const ChatInput = ({ placeholder }: ChatInputProps) => {
   const editorRef = useRef<Quill | null>(null);
 
+  const handleSubmit = ({
+    body,
+    image,
+  }: {
+    body: string;
+    image: File | null;
+  }) => {
+    console.log(body, image);
+  };
+
   return (
     <div className="px-5 w-full">
       <Editor
         placeholder={placeholder}
-        onSubmit={() => {}}
+        onSubmit={handleSubmit}
         disabled={false}
         innerRef={editorRef}
       />
