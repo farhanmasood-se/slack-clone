40b51279f126d004c4471a2011d43b42a309078e
Farhan Masood <84273944+farhanmasood-se@users.noreply.github.com>
2024-10-01 21:59:38 +0500

    implement toolbar component in message component

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -5,6 +5,7 @@ import { format, isToday, isYesterday } from 'date-fns';
 import Hint from './hint';
 import { Avatar, AvatarImage, AvatarFallback } from './ui/avatar';
 import Thumbnail from './thumbnail';
+import Toolbar from './toolbar';
 
 const renderer = dynamic(() => import('./renderer'), { ssr: false });
 
@@ -120,6 +121,17 @@ const Message = ({
           ) : null}
         </div>
       </div>
+      {!isEditing && (
+        <Toolbar
+          isAuthor={isAuthor}
+          isPending={false}
+          handleEdit={() => setEditingId(id)}
+          handleThread={() => {}}
+          handleDelete={() => {}}
+          handleReaction={() => {}}
+          hideThreadButton={hideThreadButton}
+        />
+      )}
     </div>
   );
 };
