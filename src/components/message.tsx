4531c6263f9f9e16a39da3e2fb34d07ec3699488
Farhan Masood <84273944+farhanmasood-se@users.noreply.github.com>
2024-10-04 18:30:37 +0500

    implement changes after fixing convex function and remove unused imports

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -14,8 +14,8 @@ import { useConfirm } from '@/hooks/use-confirm';
 import { useToggleReaction } from '@/features/reactions/api/use-toggle-reaction';
 import Reactions from './reactions';
 import { usePanel } from '@/hooks/use-panel';
+import ThreadBar from './thread-bar';
 
-const renderer = dynamic(() => import('./renderer'), { ssr: false });
 const Editor = dynamic(() => import('./editor'), { ssr: false });
 
 interface MessageProps {
@@ -40,6 +40,7 @@ interface MessageProps {
   hideThreadButton?: boolean;
   threadCount?: number;
   threadImage?: string;
+  threadName?: string;
   threadTimestamp?: number;
 }
 
@@ -70,6 +71,7 @@ const Message = ({
   hideThreadButton,
   threadCount,
   threadImage,
+  threadName,
   threadTimestamp,
 }: MessageProps) => {
   const { parentMessageId, onOpenMessage, onClose } = usePanel();
@@ -178,6 +180,13 @@ const Message = ({
                   </span>
                 ) : null}
                 <Reactions data={reactions} onChange={handleReaction} />
+                <ThreadBar
+                  count={threadCount}
+                  image={threadImage}
+                  name={threadName}
+                  timestamp={threadTimestamp}
+                  onClick={() => onOpenMessage(id)}
+                />
               </div>
             )}
           </div>
@@ -252,6 +261,13 @@ const Message = ({
                 <span className="text-muted-foreground text-xs">(edited)</span>
               ) : null}
               <Reactions data={reactions} onChange={handleReaction} />
+              <ThreadBar
+                count={threadCount}
+                image={threadImage}
+                name={threadName}
+                timestamp={threadTimestamp}
+                onClick={() => onOpenMessage(id)}
+              />
             </div>
           )}
         </div>
