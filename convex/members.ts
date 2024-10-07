573d932e89b8ca7bbb00048620679ce699dc1c8a
Farhan Masood <84273944+farhanmasood-se@users.noreply.github.com>
2024-10-07 15:00:33 +0500

    create remove member convex function

--- convex/members.ts
+++ convex/members.ts
@@ -147,3 +147,77 @@ export const update = mutation({
     return args.id;
   },
 });
+
+export const remove = mutation({
+  args: {
+    id: v.id('members'),
+  },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      throw new Error('Unauthorized');
+    }
+
+    const member = await ctx.db.get(args.id);
+
+    if (!member) {
+      throw new Error('Member not found');
+    }
+
+    const currentMember = await ctx.db
+      .query('members')
+      .withIndex('by_workspace_id_user_id', (q) =>
+        q.eq('workspaceId', member.workspaceId).eq('userId', userId),
+      )
+      .unique();
+
+    if (!currentMember) {
+      throw new Error('Unauthorized');
+    }
+
+    if (member.role === 'admin') {
+      throw new Error('Admin cannot be removed');
+    }
+
+    if (currentMember._id === args.id && currentMember.role === 'admin') {
+      throw new Error('Cannot remove yourself');
+    }
+
+    const [messages, reactions, conversations] = await Promise.all([
+      ctx.db
+        .query('messages')
+        .withIndex('by_member_id', (q) => q.eq('memberId', args.id))
+        .collect(),
+      ctx.db
+        .query('reactions')
+        .withIndex('by_member_id', (q) => q.eq('memberId', args.id))
+        .collect(),
+      ctx.db
+        .query('conversations')
+        .filter((q) =>
+          q.or(
+            q.eq(q.field('memberOneId'), member._id),
+            q.eq(q.field('memberTwoId'), member._id),
+          ),
+        )
+        .collect(),
+    ]);
+
+    for (const message of messages) {
+      await ctx.db.delete(message._id);
+    }
+
+    for (const reaction of reactions) {
+      await ctx.db.delete(reaction._id);
+    }
+
+    for (const conversation of conversations) {
+      await ctx.db.delete(conversation._id);
+    }
+
+    await ctx.db.delete(args.id);
+
+    return args.id;
+  },
+});
